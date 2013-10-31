340e024258d424a0bac6f8a2e4b6805f21708ab9
Blythe Sheldon <blythe.sheldon@gmail.com>
2013-10-31 17:19:59 -0400

    add modified db

--- db/migrate/20131031210408_create_blocked_times.rb
+++ db/migrate/20131031210408_create_blocked_times.rb
@@ -1,7 +1,11 @@
 class CreateBlockedTimes < ActiveRecord::Migration
-  def up
-  end
-
-  def down
+  def change
+    create_table :blocked_times do |t|
+      t.references :user
+      t.references :page
+      t.datetime :start_time
+      t.text :reason
+      t.timestamps
+    end
   end
 end
